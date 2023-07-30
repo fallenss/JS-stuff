db2c478fb818fe37f11962634eace052a2f6a53d
fallenss <volt.is.i@yandex.com>
2023-07-30 19:58:16 +0300

    parse

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
-let str1 = 'xxx';
-let str2 = 'yyy';
-let txt  = `aaa ${str1} bbb ${str2} ccc`;
-console.log(1488, str1);
-console.log(str1,str2, 1444,txt);
-alert(ууу);
+let a = parseFloat('12px');
+let b = parseFloat('3.5px');
+console.log(a+b+'px');
+alert(a+b+'px');
