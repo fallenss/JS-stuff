dbf9759674446b3999c6023a2468ce99fb8a4d63
fallenss <volt.is.i@yandex.com>
2023-08-03 19:34:21 +0300

    prompt2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 "use strict";
-let a = prompt('Name?')
-alert(a);
+let num1 = parseFloat(prompt('Введите первое число'));
+let num2 = parseFloat(prompt('Введите второе число'));
+
+alert(num1 + num2); // сложит числа как строки
+
+ num1 = parseFloat(prompt('Сторона квадрата'));
+
+alert('Площадь: '+num1 * num1); // сложит числа как строки
